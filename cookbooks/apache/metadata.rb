ec7b8631c95cb9a7fbe5b6619990c6ed7d3c5afa
krpratap451 <krpratap451@gmail.com>
2016-10-22 01:04:54 +0530

    added chef code

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'apache'
+maintainer       'pratap'
+maintainer_email 'krpratap451@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
